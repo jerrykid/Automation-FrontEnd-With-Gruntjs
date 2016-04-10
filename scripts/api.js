ab283658d806d07b0d4d1f4b6654a36c279a9c30
jerrykid <ngdinhthi@gmail.com>
2016-04-10 13:38:50 +0700

    Add unit test

---
+++ scripts/api.js
@@ -0,0 +1,27 @@
+﻿function post(url)
+{
+    var https = require('https');
+    var options = {
+        host: 'api',
+        path: url,
+        method: 'POST'        
+    };
+    var str = '';
+    https.request(options, function (response) {
+        response.on('data', function (data) {
+            str += data;
+        });
+        
+        response.on('end', function () {
+            console.log(str);            
+        });
+        
+        response.on('error', function (error) {
+            console.log(error);            
+        });
+    });    
+}  
+    
+exports.saveUser = function (user){    
+    post("/saveUser",user);
+}
---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+﻿exports.sanitize = function (word) {
+    console.log('Run Sanitize');
+    return word.toLowerCase().replace(/-/, ' ');
+};
+
+exports.tokernize = function (word) {
+    return word.split(' ');
+};
+
+exports.info = function (callback) {
+    var https = require('https');
+    var options = {
+        host: 'api.github.com',
+        path: '/repos/sayanee/build-podcast',
+        method: 'GET',
+		headers:{
+			'user-agent':'sayanee'
+		}
+    };
+    var str = '';
+    https.request(options, function (response) {
+        response.on('data', function (data) {
+            str += data;
+			//console.log(data);
+        });
+        
+        response.on('end', function () {
+            //console.log(str);
+            callback(JSON.parse(str))
+        });
+        
+        response.on('error', function (error) {
+            //console.log(error);
+            callback(error);
+        });
+    }).end();
+}
+
+exports.infoLanguage = function(infoFunc, callback){
+	infoFunc(function(reply){
+		callback('Language is ' + reply.language);
+	})
+}
+
+
